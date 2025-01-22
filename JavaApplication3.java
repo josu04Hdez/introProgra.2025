4ae70cf98165baf3412331ae7ada57a7fb7a7ddd
josu04Hdez <josuedmh1104@gmail.com>
2025-01-22 15:13:31 -0600

    Primer commit

---
+++ JavaApplication3.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author josue
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    color: #333;
+}
+h1 {
+    color: #007acc;
+}
+
+    }
+    
+}
