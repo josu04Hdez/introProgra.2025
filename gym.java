4eb71ec37c5802d001d7aeec44fdbd2b04241c91
josu04Hdez <josuedmh1104@gmail.com>
2025-02-12 20:50:13 -0600

    practicaSemana5

---
+++ gym.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NuevosMiembros josue = new NuevosMiembros();
+        josue.setNombre("josue");
+        josue.id = "04";
+        
+        nombreMiembro christhopher = new nombreMiembro);
+        christhopher.setNombre("christhopher");
+        christhopher.id = "20";
+        
+        JOptionPane.showMessageDialog(null, josue); 
+        JOptionPane.showMessageDialog(null, josue.getId());  
+        
+        JOptionPane.showMessageDialog(null, christhopher); 
+        JOptionPane.showMessageDialog(null, christhopher.getId()); 
+        
+        Entrenadores horarioClase= new Carrera();
+        horarioClase.Entrenadores = "Mañana";
+        
+    }
+}
